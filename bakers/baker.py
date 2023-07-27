38e9df4e8387ef37931c617af5656910578eb6e7
unknown <Lab@cns-dd2shydz1.ou.ad3.ucdavis.edu>
2023-07-26 17:00:54 -0700

    fixing bug in var_length. var_length will be stored in default_stim

--- bakers/baker.py
+++ bakers/baker.py
@@ -22,14 +22,20 @@ def check_flags(tuning_functions, default_stim):
     for index in range(len(tuning_functions)):
         current_dic = tuning_functions[index]
         current_default = current_dic['default']
+
         # list of all defined default variables in the current dic
         current_var = current_dic['default']
         current_var = [sublist[0] for sublist in current_var]
         # add to this the list of tuned variables
         current_var = set(current_var + current_dic['tuning_type'])
         missing_list = list(set(unique_values_list) - set(current_var))
-        missing_values = [getattr(default_stim, param) for param in missing_list]
-        current_default.extend([[param, getattr(default_stim, param)] for param in missing_list])
+
+        #missing_list = [item.replace('-', '') for item in missing_list]
+
+        # missing_values = [getattr(default_stim, param) for param in missing_list]
+        current_default.extend([[param, getattr(default_stim, param.replace('-', ''))[0]] for param in missing_list])
+        print(current_default)
+        breakpoint()
         tuning_functions[index]['default'] = current_default
     return tuning_functions
 
@@ -43,22 +49,21 @@ def combine_config_strings(config_list, flag_list):
         for tc_index, config in enumerate(config_list):
 
             if var_config == '':
-                flag_pattern = r'(-{1}' + flag + r'(\s?-?\d+\.?\d*,?)*)'
+                flag_pattern = r'(' + flag + r'(\s?-?\d+\.?\d*,?)*)'
+
                 current_config = re.findall(flag_pattern, config)[0][0]
                 if current_config:
                     var_config += current_config
 
             else:
-                flag_pattern = r'(-{1}' + flag + ')' + r'(\s?-?\d+\.?\d*,?)*'
-                # pattern = r'(-{1,2}\D+ .*?)(?=-\D|$)'
-                current_flag = r'-{1}' + flag
+                current_flag = flag
                 pattern = current_flag + r'(.*?)(?=-\D|$)'
                 current_config = re.findall(pattern, config)
                 current_config = ',' + current_config[0]
                 current_config = current_config.replace(" ","")
                 if current_config:
                     var_config += current_config
-        # breakpoint()
+
         print(f'var_config =  {var_config}')
 
         if var_config != '':
@@ -80,12 +85,14 @@ def randomize_trials(input_string, condition_string, var_length, num_trials):
     flag_values = {}
 
     # Group the matches by flag
-    flag_pattern = r'(-{1,2}[^-\d])(.+)'
+    #flag_pattern = r'(-{1,2}[^-\d])(.+)'
+    flag_pattern = r'(-{1,2}[a-zA-Z]+)\s(.+)'
 
     for match, current_var_length in zip(matches, var_length):
 
         flag_matches = re.findall(flag_pattern, match)
-
+        print(f" match = {match}")
+        print(f'flag_matches = {flag_matches}')
         flag = flag_matches[0][0]
         flag = flag.rstrip()
         values = flag_matches[0][1].split(',')
@@ -96,16 +103,16 @@ def randomize_trials(input_string, condition_string, var_length, num_trials):
             flag_values[flag].append(values)
         else:
             flag_values[flag] = values
-
     # Randomize the order of values within each flag
 
     indices = np.random.permutation(num_trials)
     condition_string = [condition_string[i] for i in indices]
-
     for flag in flag_values:
         print(f"current flag  = {flag}")
+        print(f"flag_values =  {flag_values[flag]}")
         flag_values[flag] = [flag_values[flag][i] for i in indices]
         flag_values[flag] = flatten_list(flag_values[flag])
+
 
 
     # Create a new string with randomized flag-value pairs
@@ -117,7 +124,6 @@ def randomize_trials(input_string, condition_string, var_length, num_trials):
 
         values_str = values_str.replace(" ", "")
         new_string += f"{flag} {values_str} "
-
 
     new_string += '"'
 
@@ -168,15 +174,16 @@ class Bakers:
 
 class Stimulus:
     def __init__(self, **kwargs):
-        self.C = kwargs.get('C', 100.0)
-        self.A = kwargs.get('A', 5.0)
-        self.S = kwargs.get('S', 1.0)
-        self.T = kwargs.get('T', 5.0)
-        self.O = kwargs.get('0', 90.0)
-        self.P = kwargs.get('P', 0.0)
-        self.Z = kwargs.get('Z', [0.0, 0.0])
-        self.sweep = kwargs.get('sweep', [1.0, 0.0, 5.0])
-        self.wh = kwargs.get('wh', [5.0, 5.0])
+        self.C = kwargs.get('-C', [[100.0], 1])
+        self.A = kwargs.get('-A', [[5.0], 1])
+        self.S = kwargs.get('-S', [[1.0], 1])
+        self.T = kwargs.get('-T', [[5.0], 1])
+        self.O = kwargs.get('-0', [[90.0], 1])
+        self.P = kwargs.get('-P', [[0.0], 1])
+        self.Z = kwargs.get('-Z', [[0.0, 0.0], 2])
+        self.sweep = kwargs.get('--sweep', [[1.0, 0.0, 5.0], 3])
+        self.wh = kwargs.get('--wh', [[5.0, 5.0], 2])
+
 
     def print(self):
         for attr, value in self.__dict__.items():
@@ -190,7 +197,6 @@ class TuningFunction:
         self.config_string = ''
         self.tuning_params = tuning_params['param_type']
         self.default_params = tuning_params['default']
-        self.var_length = tuning_params['var_length']
 
         if tuning_params['param_type'] == 'param':
             for tuning_index in range(len(self.tuning_type)):
@@ -219,16 +225,17 @@ class TuningFunction:
                 self.iv[tuning_index] = tuning_params['values'][tuning_index]
 
             self.combo_list = list(zip(*self.iv))
-            # breakpoint()
+
 
 
         else:
             raise ValueError("Invalid condition found")
         # print(len(self.combo_list ))
-        # breakpoint()
+        breakpoint()
         self.num_trials = len(self.combo_list)
         self.condition = tuning_params['condition'] * len(self.combo_list)
         self.generate_config_string()
+        breakpoint()
 
     def generate_config_string(self):
         for element_id in range(len(self.combo_list[0])):
@@ -238,7 +245,7 @@ class TuningFunction:
             result_string = ', '.join("{:.2f}".format(item) for item in new_list)
             result_string = result_string.replace(" ", "")
             n = len(new_list)
-            self.config_string += f"-{self.tuning_type[element_id]} {result_string} "
+            self.config_string += f"{self.tuning_type[element_id]} {result_string} "
 
         if self.default_params is not None:
 
@@ -248,11 +255,11 @@ class TuningFunction:
                 current_values = [current_values] * n
                 new_string = ", ".join(str(element) for element in current_values)
                 new_string = new_string.replace(" ", "")
-                self.config_string += f"-{current_flag} {new_string} "
-                if type(current_values[0]) == list:
-                    self.var_length.append(len(current_values[0]))
-                else:
-                    self.var_length.append(1)
+                self.config_string += f"{current_flag} {new_string} "
+                # if type(current_values[0]) == list:
+                #     self.var_length.append(len(current_values[0]))
+                # else:
+                #     self.var_length.append(1)
         self.config_string = self.config_string.replace("[", "").replace("]", "")
         self.config_string = self.config_string.replace("(", "").replace(")", "")
 
